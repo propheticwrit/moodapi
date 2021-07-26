aaf3e683c39382d2cb9571ee12fba9190bb4e465
propheticwrit <job@vitalflohealth.com>
2021-07-26 08:27:07 -0400

    Add question view and serializer.

---
+++ api/serializers/question.py
@@ -0,0 +1,13 @@
+from rest_framework.serializers import ModelSerializer
+
+from api.models import Question
+from api.serializers.answer import AnswerSerializer
+
+
+class QuestionSerializer(ModelSerializer):
+
+    answers = AnswerSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Question
+        fields = ['id', 'name', 'text', 'created', 'modified', 'survey', 'answers']
---
+++ api/views/question.py
@@ -0,0 +1,10 @@
+from rest_framework.viewsets import ModelViewSet
+
+from api.models import Question
+from api.serializers.question import QuestionSerializer
+
+
+class QuestionViewSet(ModelViewSet):
+
+    serializer_class = QuestionSerializer
+    queryset = Question.objects.all()
