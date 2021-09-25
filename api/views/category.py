3aa0902f3b029c766022602e26bd0c3d4801e150
propheticwrit <job@vitalflohealth.com>
2021-09-25 16:41:59 -0400

    Return a dictionary with the null parent categories (base categories) and a list of their children.
    {'{parent name}': {parent_dict}, 'children': [{children_list}]}

--- api/views/category.py
+++ api/views/category.py
@@ -1,3 +1,5 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from api.mixins import APIMixin
@@ -8,5 +10,23 @@ from api.serializers.category import CategorySerializer
 class CategoryViewSet(APIMixin, ModelViewSet):
 
     serializer_class = CategorySerializer
-    queryset = Category.objects.all()
-    filterset_fields = ['user']
+    queryset = Category.objects.all().order_by('parent_id')
+    filterset_fields = ['user', 'parent_id']
+
+    @action(detail=False, url_path='base')
+    def base_categories(self, request):
+
+        null_parents = Category.objects.filter(parent_id=None)
+
+        null_parent_ids = null_parents.values_list('id', flat=True)
+
+        null_children = self.queryset.filter(parent_id__in=null_parent_ids)
+
+        base_categories = []
+        for null_parent in null_parents:
+            null_parent_dict = {null_parent.name: self.serializer_class(null_parent).data, 'children': []}
+            for null_child in null_children.filter(parent_id=null_parent.id):
+                null_parent_dict['children'].append(self.serializer_class(null_child).data)
+            base_categories.append(null_parent_dict)
+
+        return Response(base_categories)
